03f6fc31a0cf09b4240f3b090fe1d47ae7900bcc
Gaël Gahimbare <gaelgahimbare@yahoo.fr>
2021-02-17 11:32:54 -0500

    Ajout de la classe EmployeUniversite

--- Professeur.js
+++ Professeur.js
@@ -14,9 +14,18 @@ class EmployeUniversite {
         return this.prenom + ", " + this.nom;
     }
 
-    information() {
-        return this.numeroEmploye + " - "
-            + this.nomComplet() +
-            " - Ancienneté: " + this.anneesExperience;
+}
+
+
+
+class EmployeUniversiteProfesseur extends EmployeUniversite {
+    constructor(nom, prenom, numeroEmploye, anneesExperience, salaire, coursEnseigne) {
+        super(nom, prenom, numeroEmploye, anneesExperience);
+        this.salaire = salaire;
+        this.coursEnseigne = coursEnseigne;
+    }
+
+    ajouterUnCours(nouveauCours) {
+        this.coursEnseigne[this.coursEnseigne.length] = nouveauCours;
     }
 }
